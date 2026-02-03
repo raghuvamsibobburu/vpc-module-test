e8583a9730a0f1d7dc6db7a4ad175eeef665ebbd
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2026-02-03 22:58:33 +0530

    v2

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-module "ec2" {
-    source = "../terraform-aws-vpc"
-    project = "roboshop"
+module "vpc" {
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/daws-84s/terraform-aws-vpc.git?ref=main"
+    /* project = "roboshop"
     environment = "dev"
-    public_subnet_cidr = [ "10.0.1.0/24", "10.0.1.0/24" ]
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
+    project = var.project
+    environment = var.environment
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 }
